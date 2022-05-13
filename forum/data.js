bb4a126ed0db6da8e493577e8b933bd36e5a972b
WeirdOddz <92236779+WeirdOddz@users.noreply.github.com>
2022-05-13 15:52:02 +1000

    forumTest

---
+++ forum/data.js
@@ -0,0 +1,20 @@
+var threads = [
+    {
+        title: "Thread 1",
+        author: "Oscar",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Oscar",
+                date: Date.now(),
+                content: "Hey There, what is the answer to the question I did not ask?"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Y E S"
+            }
+        ]
+    }
+]
